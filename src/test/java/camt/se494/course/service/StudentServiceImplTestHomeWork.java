8dd57a7f9cc4fa0369b99dffe2bf317fa5d51d36
Jiramed <jiramed_wi@cmu.ac.th>
2021-08-29 17:28:47 +0700

    Do the first test on student GPA

--- src/test/java/camt/se494/course/service/StudentServiceImplTestHomeWork.java
+++ src/test/java/camt/se494/course/service/StudentServiceImplTestHomeWork.java
@@ -1,17 +1,58 @@
 package camt.se494.course.service;
 
+import camt.se494.course.entity.CourseEnrolment;
+import camt.se494.course.entity.Student;
+import camt.se494.course.service.util.GradeMatcher;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.number.IsCloseTo.*;
+
 
 class StudentServiceImplTestHomeWork {
+    Student student1 = null;
+//    CourseDao courseDao = null;
+//    OpenedCourseDao openedCourseDao = null;
+    GradeMatcher gradeMatcher = null;
+
+    @BeforeEach
+    void initstudent() {
+        //set 1st student
+        student1 = mock(Student.class);
+        when(student1.getStudentId()).thenReturn("5001");
+        when(student1.getName()).thenReturn("Kong");
+        //set course for 1st student
+        List<CourseEnrolment> courseEnrolmentList = new ArrayList<>();
+        CourseEnrolment courseEnrolment0 = mock(CourseEnrolment.class);
+        when(courseEnrolment0.getGrade()).thenReturn("A");
+        courseEnrolmentList.add(courseEnrolment0);
+        CourseEnrolment courseEnrolment1 = mock(CourseEnrolment.class);
+        when(courseEnrolment1.getGrade()).thenReturn("B+");
+        courseEnrolmentList.add(courseEnrolment1);
+        CourseEnrolment courseEnrolment2 = mock(CourseEnrolment.class);
+        when(courseEnrolment2.getGrade()).thenReturn("B");
+        courseEnrolmentList.add(courseEnrolment2);
+        when(student1.getCourseEnrolments()).thenReturn(courseEnrolmentList);
+        //Set gradematcher
+        gradeMatcher = mock(GradeMatcher.class);
+        when(gradeMatcher.getGradeScore("A")).thenReturn(4.00);
+        when(gradeMatcher.getGradeScore("B+")).thenReturn(3.50);
+        when(gradeMatcher.getGradeScore("B")).thenReturn(3.00);
+    }
 
     @Test
     void getStudent() {
     }
 
     @Test
-    void testGetStudent() {
+    void getStudentPartial() {
     }
 
     @Test
@@ -24,10 +65,13 @@ class StudentServiceImplTestHomeWork {
 
     @Test
     void getStudentGpa() {
+        StudentServiceImpl studentService = new StudentServiceImpl();
+        studentService.setGradeMatcher(gradeMatcher);
+        assertThat(studentService.getStudentGpa(student1),is(closeTo(3.50,0.001)));
     }
 
     @Test
-    void testGetStudentGpa() {
+    void getStudentGpaAcademicYear() {
     }
 
     @Test
