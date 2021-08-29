34baf50645ce6b600f664a7c9817448d04539ca3
Jiramed <jiramed_wi@cmu.ac.th>
2021-08-29 13:51:54 +0700

    Create the test file

---
+++ src/test/java/camt/se494/course/service/CourseServiceImplTest.java
@@ -0,0 +1,32 @@
+package camt.se494.course.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServiceImplTest {
+
+    @Test
+    void getCourse() {
+    }
+
+    @Test
+    void testGetCourse() {
+    }
+
+    @Test
+    void testGetCourse1() {
+    }
+
+    @Test
+    void testGetCourse2() {
+    }
+
+    @Test
+    void getCourseReport() {
+    }
+
+    @Test
+    void getCourseGpa() {
+    }
+}
---
+++ src/test/java/camt/se494/course/service/StudentServiceImplTestHomeWork.java
@@ -0,0 +1,40 @@
+package camt.se494.course.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceImplTestHomeWork {
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void testGetStudent() {
+    }
+
+    @Test
+    void getStudentGradeLowerThan() {
+    }
+
+    @Test
+    void getStudentGradeGreaterThan() {
+    }
+
+    @Test
+    void getStudentGpa() {
+    }
+
+    @Test
+    void testGetStudentGpa() {
+    }
+
+    @Test
+    void getStudentReport() {
+    }
+
+    @Test
+    void getRegisterYear() {
+    }
+}
